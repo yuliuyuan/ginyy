e401aeda77b924742d0300583974b2882ec885eb
yuliuyuan <yuliuyuan@bytedance.com>
2021-07-14 20:24:18 +0800

    断点
    Change-Id: I5044f2623b6eb3a12c0aa7302ce78272227a50f0

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
-module github/ginyy
+module github/goweb
 
 go 1.16
+require ginyy v0.0.0
+
+replace ginyy => ./gee
--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github/ginyy/ginyy"
 	"net/http"
 )
 
+type Engine struct{}
+
+
 func main() {
-	http.HandleFunc("/",solvePing)
-	http.ListenAndServe(":9999",nil)
+	engine := ginyy.New()
+	engine.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
+	engine.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+	engine.Run(":9999")
 }
 
-func solvePing(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "hello world!")
-}
